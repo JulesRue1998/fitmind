6d9c6727bdee3e459a03c2be7a6d8806e9c3959c
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-19 12:27:42 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -13,9 +13,9 @@ if page == "Fitness":
     st.write("If you're new to exercise or a particular exercise, start with 1 to 2 sets per exercise. Focus on learning proper form and gradually increasing the number of sets as you become more comfortable with the movements.")
     st.subheader("Intermediate")
     st.write("Intermediate: For those who have been exercising regularly and have some experience with the exercises, aim for 3 to 4 sets per exercise. This provides enough volume to challenge your muscles and promote strength and muscle growth.")
-    st.subheadere("Advanced")
+    st.subheader("Advanced")
     st.write("Advanced individuals who are looking to increase muscle size or strength may benefit from performing 4 to 5 sets per exercise. Higher volume workouts can help stimulate muscle hypertrophy and strength gains.")
-
+    st.title("workouts")
     st.subheader("Squats")
     st.write("Stand with your feet shoulder-width apart, lower your body by bending your knees, and then return to the starting position.")
     st.write("Squats primarily target the legs and glutes.")
