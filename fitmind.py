47bb3aee3f335d365fcf07c05d27e2f5b5a23040
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-19 18:57:16 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -13,20 +13,15 @@ if page == "FitMind - Introduction":
 
 elif page == "Fitness":
     st.title("Fitness")
-    with st.expander("See explanation"):
-        st.write(\"\"\" Levels
-                Beginners
-                Intermediate
-                Advanced
-    \"\"\")
-    st.title("workouts")
+    
     st.subheader("Beginners")
     st.write("If you're new to exercise or a particular exercise, start with 1 to 2 sets per exercise. Focus on learning proper form and gradually increasing the number of sets as you become more comfortable with the movements.")
     st.subheader("Intermediate")
     st.write("Intermediate: For those who have been exercising regularly and have some experience with the exercises, aim for 3 to 4 sets per exercise. This provides enough volume to challenge your muscles and promote strength and muscle growth.")
     st.subheader("Advanced")
     st.write("Advanced individuals who are looking to increase muscle size or strength may benefit from performing 4 to 5 sets per exercise. Higher volume workouts can help stimulate muscle hypertrophy and strength gains.")
-
+   
+    st.title("workouts")
     st.subheader("Choose your Workouts")
 
     st.subheader("Squats")
