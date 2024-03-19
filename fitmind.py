bc61808e706bc93e7b468b63336cc7f535821e30
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-19 17:39:34 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -16,13 +16,14 @@ if page == "Fitness":
     st.write("Intermediate: For those who have been exercising regularly and have some experience with the exercises, aim for 3 to 4 sets per exercise. This provides enough volume to challenge your muscles and promote strength and muscle growth.")
     st.subheader("Advanced")
     st.write("Advanced individuals who are looking to increase muscle size or strength may benefit from performing 4 to 5 sets per exercise. Higher volume workouts can help stimulate muscle hypertrophy and strength gains.")
-    st.title("workouts")
-
     
-popover = st.popover("Filter items")
-Glutes = popover.checkbox("Squats", True)
-Arms = popover.checkbox("Pushups", True)
-
+    st.title("workouts")
+    
+    
+    popover = st.popover("Filter items")
+    Glutes = popover.checkbox("Squats", True)
+    Arms = popover.checkbox("Pushups", True)
+    
     st.subheader("Squats")
     st.write("Stand with your feet shoulder-width apart, lower your body by bending your knees, and then return to the starting position.")
     st.write("Squats primarily target the legs and glutes.")
