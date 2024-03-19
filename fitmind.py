23c44e820d4cb122b2bbbded8947bfa39955ee7e
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-19 18:00:51 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -8,32 +8,16 @@ page = st.sidebar.radio("Navigation",["Fitness", "Mental Health"])
 
 if page == "Fitness":
     st.title("Fitness")
+    
+    st.title("workouts")
     st.subheader("Beginners")
     st.write("If you're new to exercise or a particular exercise, start with 1 to 2 sets per exercise. Focus on learning proper form and gradually increasing the number of sets as you become more comfortable with the movements.")
     st.subheader("Intermediate")
     st.write("Intermediate: For those who have been exercising regularly and have some experience with the exercises, aim for 3 to 4 sets per exercise. This provides enough volume to challenge your muscles and promote strength and muscle growth.")
     st.subheader("Advanced")
     st.write("Advanced individuals who are looking to increase muscle size or strength may benefit from performing 4 to 5 sets per exercise. Higher volume workouts can help stimulate muscle hypertrophy and strength gains.")
-    
-    st.title("workouts")
-    
-    popover = st.popover("Filter items")
-    Glutes = popover.checkbox("Squats", True)
-    Arms = popover.checkbox("Pushups", True)
 
-if Glutes:
-    st.write(":Glutes[st.subheader('Squats'),
-    st.write('Stand with your feet shoulder-width apart, lower your body by bending your knees, and then return to the starting position.'),
-    st.write('Squats primarily target the legs and glutes.')
-    ]")
-    
-if Arms:
-    st.write(":Arms[This is a blue item.]")
-    
-    st.subheader("Squats")
-    st.write("Stand with your feet shoulder-width apart, lower your body by bending your knees, and then return to the starting position.")
-    st.write("Squats primarily target the legs and glutes.")
-    st.subheader("Pushups")
+    st.write("Choose your Workouts")
 
 elif page == "Mental Health":
     st.title("Mental Health")
