9149627639b4b3b8f0a7cd130591e4fd32497464
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-19 12:01:53 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -12,9 +12,4 @@ st.sidebar.title("Menu")
 
 page = st.sidebar.radio("FitMind", ["Fitness", "Mental Health"])
 
-if page == "Fitness":
-    st.title("Fitness")
-    st.write("Hier können Sie Informationen über unsere Anwendung finden.")
-elif page == "Mental Health":
-    st.title("Mental Health")
-    st.write("Hier finden Sie Informationen über unser Team und unsere Mission.")
+
