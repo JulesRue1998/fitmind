e4da7c4065b790a25fbbef07c1a2c03818701c46
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-19 12:00:47 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -1,7 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Hello World')
+st.title('FitMind')
 
-st.sidebar.title('Menu')
+st.subtitle('Fitness')
 
+st.subtitle('Mental Health')
+
+
+st.sidebar.title("Menu")
+
+page = st.sidebar.radio("FitMind", ["Fitness", "Mental Health"])
+
+# Abhängig von der ausgewählten Seite wird der entsprechende Inhalt angezeigt
+if page == "Fitness":
+    st.title("Willkommen auf der Startseite")
+    st.write("Hier können Sie Informationen über unsere Anwendung finden.")
+elif page == "Mental Health":
+    st.title("Mental Health")
+    st.write("Hier finden Sie Informationen über unser Team und unsere Mission.")
