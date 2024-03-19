f747fd2bf8454495804ee4ed726e6c1c9be69495
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-19 12:02:41 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -3,13 +3,19 @@ import pandas as pd
 
 st.title('FitMind')
 
-st.subtitle('Fitness')
+st.subheader('Fitness')
 
-st.subtitle('Mental Health')
+st.subheader('Mental Health')
 
 
 st.sidebar.title("Menu")
 
 page = st.sidebar.radio("FitMind", ["Fitness", "Mental Health"])
 
+if page == "Fitness":
+    st.title("Fitness")
+    st.write("Hier können Sie Informationen über unsere Anwendung finden.")
+elif page == "Mental Health":
+    st.title("Mental Health")
+    st.write("Hier finden Sie Informationen über unser Team und unsere Mission.")
 
