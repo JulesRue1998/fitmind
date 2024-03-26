e5f62d5bce8804fb032a2d584030800e3e0fd156
cherilynh <162223972+cherilynh@users.noreply.github.com>
2024-03-26 11:51:47 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
-from PIL import Image
 
 st.sidebar.title("Menu")
 page = st.sidebar.radio("Choose what you need",["FitMind - Introduction", "Fitness", "Mental Health", "Food & Recipes"])
