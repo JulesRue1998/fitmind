a6780ceb9e5457719290b9cbe59b7cf2ab719128
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-26 14:42:26 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -101,15 +101,15 @@ elif page == "Mental Health":
      Mental_Health_Subcategories = [" ", "Stresslevel tracker ", "Mood tracker", "Sleep tracker"]
      selected_subcategory = st.sidebar.selectbox("Choose a tracker", Mental_Health_Subcategories)
   
-    if selected_subcategory == " ":
+    if selected_subcategory == "":
         st.write(" ")
-
+    
     elif selected_subcategory == "Stresslevel tracker":
         st.write("Track your Stresslevels")
-
+    
     elif selected_subcategory == "Mood tracker":
         st.write("Track your mood")
-
+    
     elif selected_subcategory == "Sleep tracker":
         st.write("track your sleeping hours")
 
