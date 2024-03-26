160a816d705d7e8ecd33129a5fa314285fdb2d69
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-03-26 22:29:55 +0100

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -169,16 +169,15 @@ elif page == "Fitness":
         st.write(" ")
         
     elif third_subcategory == "Water intake":
-        
         if "water_intake" not in st.session_state:
-                st.session_state.water_intake = 0
-            
+        st.session_state.water_intake = 0
+                
         water_emoji = "💧"
         st.write("track your water intake here! for every glass of water, click a button!", water_emoji)    
         if st.button(water_emoji):
-                st.session_state.water_intake += 1
-                st.write("You drank a glass of water!")
-                st.write("Total glasses of water drank today:", st.session_state.water_intake)
+        st.session_state.water_intake += 1
+        st.write("You drank a glass of water!")
+        st.write("Total glasses of water drank today:", st.session_state.water_intake)
 
         
     elif third_subcategory == "calorie traker":
