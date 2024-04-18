d530bc9169f3881e8624efd83b02f749a28792d5
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-04-18 22:14:19 +0200

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -701,13 +701,13 @@ elif page == "Fitness":
                 
             # Show a spinner during a process
             with st.spinner(text='In progress'):
-            time.sleep(3)
-            st.success('Done')
+                time.sleep(3)
+                st.success('Done')
 
             # Show and update progress bar
             bar = st.progress(50)
-            time.sleep(3)
-            bar.progress(100)
+                time.sleep(3)
+                bar.progress(100)
         
         with tab2:
             st.header("Intermediate Training")
