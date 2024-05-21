f63e47a188df1cfe5e044b9a2ae4b0c43b5d198a
JulesRue1998 <161706334+JulesRue1998@users.noreply.github.com>
2024-05-21 13:44:09 +0200

    Update fitmind.py

--- fitmind.py
+++ fitmind.py
@@ -901,17 +901,17 @@ elif page == "Fitness":
         st.write(" ")
         
     elif third_subcategory == "water intake":
-        st.subheader("Track your water intake :droplet:")
+        st.subheader(":blue[Track your water intake] :droplet:")
         st.write("Track your water intake. A glass has about 250ml of water in it. Aim to drink around 2.5 liters of water per day.")
         if "water_intake" not in st.session_state:
             st.session_state.water_intake = 0
         
         # Display the water emoji
         water_emoji = "💧"
-        st.write("if you drank a glass of water, press the button below! :droplet:")
+        st.write(":blue[if you drank a glass of water, press the button below!] :droplet:")
         
         # Add a button to increment the water intake counter when clicked
-        if st.button("Drink a glass of water :droplet:"):
+        if st.button(":blue[Drink a glass of water] :droplet:"):
             st.session_state.water_intake += 1
             st.write("You drank a glass of water! :droplet:")
             st.write("Total glasses of water drank today:", st.session_state.water_intake)
